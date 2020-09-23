7c4968d1ea1e13e20b4b419e53cc3bae39a603e0
blue0316 <greatblueknight@gmail.com>
2020-09-23 11:00:41 +0100

    git ignore

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ if (isset($_GET['city'])) {
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-        <title>Weather Scraper</title>
+        <title>Weather App</title>
         <style>
             html {
                 background: url("myBackground.jpg");
